72e8d3c4db38f5cd2264af75ddc482b737073015
starcoex <69073390+starcoex@users.noreply.github.com>
2022-12-30 17:49:23 +0900

    Go First Setting

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func mutiply(a int , b int) int {
+
+	return a * b
+}
+
+func main() {
+	fmt.Println(mutiply(2, 2))
+// 	age := 23
+// 	na := "Ko"
+// var name string = "Nico"
+// name = "Lyin"
+// fmt.Println(age, na)
+// fmt.Println(name)
+}
