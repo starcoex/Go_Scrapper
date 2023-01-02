54b0fdddcf30ad929aadf9396602dad511cf9516
starcoex <69073390+starcoex@users.noreply.github.com>
2023-01-02 16:15:19 +0900

    Go Func part 2

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"strings"
 )
 
-func lenAndUpper(name string)(int, string){
-return len(name), strings.ToUpper(name)
+func lenAndUpper(name string)(length int,uppercase string){
+	defer fmt.Println("I'm done")
+length = len(name)
+uppercase= strings.ToLower(name)
+return
 }
 func repeatMe(words ...string){
 	fmt.Println(words)
@@ -15,6 +18,8 @@ func repeatMe(words ...string){
 
 
 func main() {
+totalLen, up := lenAndUpper("kKKEK")
+fmt.Println(totalLen, up)
 	totalLength, upperName := lenAndUpper("nico")
 	fmt.Println(totalLength, upperName)
 	fmt.Println((lenAndUpper("Nico")))
